22947449166dd71742d642efd34d896dd1235e58
grigilo <tsoy.grigoriy@gmail.com>
2024-08-27 12:56:49 +0300

    token ttl

--- announcement/settings.py
+++ announcement/settings.py
@@ -147,3 +147,8 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
