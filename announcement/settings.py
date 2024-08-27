c40471b3206ed0266ed4ea85190d0139f778ee64
grigilo <tsoy.grigoriy@gmail.com>
2024-08-27 13:13:52 +0300

    last

--- announcement/settings.py
+++ announcement/settings.py
@@ -148,7 +148,3 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-}
